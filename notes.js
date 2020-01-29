c3c7f6f33d6d9c2525a05c49bcc47c5dc3c31179
Priyadarshan  Vijay <priyadarshan.v@talview.com>
2020-01-29 17:58:52 +0530

    Created loadNotes function

--- notes.js
+++ notes.js
@@ -1,9 +1,21 @@
+const fs = require('fs');
 const getNotes = function(){
     return "Your notes...";
 };
 
 const addNote = function(title, body){
-    
+    const notes = loadNotes();
+    console.log(notes);
+}
+
+const loadNotes = function(){
+    try{
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    }catch(e){
+        return []; // return nothing as the file is not there
+    }
 }
 
 module.exports = {
